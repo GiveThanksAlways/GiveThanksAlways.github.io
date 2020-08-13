baa3b9a2319cadf4a9c931dafd0917b8a514c93b
Willett, Spencer <spencer.willett@intel.com>
2020-08-12 20:40:58 -0700

    docker website working

--- serve.sh
+++ serve.sh
@@ -1,1 +1,9 @@
-bundle exec jekyll serve -P 8888
+#!/bin/bash
+
+echo 'this is my script now'
+bundle config force_ruby_platform true
+BUNDLE_FORCE_RUBY_PLATFORM=true
+# bundle exec jekyll serve -P 4000
+bundle install
+jekyll serve
+
