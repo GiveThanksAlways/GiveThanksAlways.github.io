aed1361a0a4bf8e5297047d6f1fc589c95496ec7
Willett, Spencer <spencer.willett@intel.com>
2021-05-06 09:29:30 -0700

    delete step

--- serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'this is my script now'
-bundle config force_ruby_platform true
-BUNDLE_FORCE_RUBY_PLATFORM=true
-# bundle exec jekyll serve -P 4000
-bundle install
-jekyll serve
-
